9414453083ae112df3c26bf4ed57f031efa54cbc
Tsy <842414797@qq.com>
2025-03-20 12:54:08 +0800

    补全"新增员工“功能。1.捕捉用户姓名重复异常，并result返回前端2.补全动态获取修改人id，使用BaseContext工具类解析 增加员工模糊搜索

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,11 +1,14 @@
 package com.sky.mapper;
 
 import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 @Mapper
 public interface EmployeeMapper {
@@ -26,4 +29,5 @@ public interface EmployeeMapper {
     void insert(Employee employee);
 
 
+    List<Employee> list(EmployeePageQueryDTO employeePageQueryDTO);
 }
